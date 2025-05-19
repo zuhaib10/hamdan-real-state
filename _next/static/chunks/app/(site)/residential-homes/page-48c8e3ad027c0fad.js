93bac7a1131902b5fda50ca766c70b4b18b18dbe
Your Name <zuhaib@mvp-apps.ae>
2025-05-19 10:16:28 +0400

    Add Serenity Height Villas property page with detailed information and images

---
+++ _next/static/chunks/app/(site)/residential-homes/page-48c8e3ad027c0fad.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[131,337,498,526,834],{9130:(e,s,n)=>{Promise.resolve().then(n.bind(n,7347)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[63,874,347,441,684,358],()=>s(9130)),_N_E=e.O()}]);
